3c4d090100f806b36b3616b4ee49c9d33c1b15c2
André <AndreR@outlook.de>
2022-04-04 17:03:34 +0200

    Yield

---
+++ HalloYield/HalloYield/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+foreach (var item in GetStrings())
+{
+    Console.WriteLine(item);
+}
+
+await foreach (var item in GetStringsAsync())
+{
+    Console.WriteLine(item);
+}
+
+IEnumerable<string> GetStrings()
+{
+    yield return "Hallo";
+    yield return "World!";
+    yield return "What's";
+    yield return "up?";
+}
+
+async IAsyncEnumerable<string> GetStringsAsync()
+{
+    yield return "Hallo";
+    await Task.Delay(1000);
+    yield return "World!";
+    await Task.Delay(1000);
+    yield return "What's";
+    await Task.Delay(1000);
+    yield return "up?";
+}
+
+
+class Person
+{
+    public int Age { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? First { get; set; }
+
+    public IEnumerable<string> Texts { get; set; } = new List<string>();
+}
