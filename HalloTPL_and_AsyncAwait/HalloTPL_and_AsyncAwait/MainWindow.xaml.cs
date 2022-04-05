1337eabcfba5bf78e3d82d857f1fea75e77d6342
André <AndreR@outlook.de>
2022-04-05 14:55:15 +0200

    WPF TPL and Asnyc/Await

--- HalloTPL_and_AsyncAwait/HalloTPL_and_AsyncAwait/MainWindow.xaml.cs
+++ HalloTPL_and_AsyncAwait/HalloTPL_and_AsyncAwait/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -43,16 +44,104 @@ namespace HalloTPL_and_AsyncAwait
             {
                 for (int i = 0; i < 100; i++)
                 {
-                    Thread.Sleep(100);
+                    Thread.Sleep(10);
                     Dispatcher.Invoke(() =>
                     {
                         Title = $"{i}";
                         pb1.Value = i;
                     });
                 }
+
                 Dispatcher.Invoke(() => b1.IsEnabled = true);
             });
 
+        }
+
+        private void StartTaskWithTs(object sender, RoutedEventArgs e)
+        {
+            var ts = TaskScheduler.FromCurrentSynchronizationContext();
+            b2.IsEnabled = false;
+            cts = new CancellationTokenSource();
+
+            var t = Task.Run(() =>
+             {
+                 for (int i = 0; i < 100; i++)
+                 {
+                     Thread.Sleep(20);
+
+                     //int ii = i;
+                     Task.Factory.StartNew(() =>
+                     {
+                         Title = $"{i}";
+                         pb1.Value = i;
+                     }, CancellationToken.None, TaskCreationOptions.None, ts);
+
+                     //if (i == 75)
+                     //throw new OutOfMemoryException();
+                     //if (cts.IsCancellationRequested) //simple
+                     //    break;//todo cleanup
+
+                     cts.Token.ThrowIfCancellationRequested(); //if you need the info, if abort was done
+                 }
+                 Dispatcher.Invoke(() => b2.IsEnabled = true);
+             });
+            t.ContinueWith(tc => MessageBox.Show("OK"), CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, ts);
+            t.ContinueWith(tc =>
+            {
+                b2.IsEnabled = true;
+                if (tc.Exception.InnerException is OperationCanceledException)
+                    MessageBox.Show("Successfully aborted");
+                else
+                    MessageBox.Show($"ERROR: {tc.Exception.InnerException.Message}");
+            }, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, ts);
+
+        }
+
+        CancellationTokenSource cts = null;
+
+        private void Abort(object sender, RoutedEventArgs e)
+        {
+            cts?.Cancel();
+        }
+
+        private async void StartAsyncAwait(object sender, RoutedEventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            for (int i = 0; i < 100; i++)
+            {
+                Title = $"{i}";
+                pb1.Value = i;
+
+                try
+                {
+                    await Task.Delay(100, cts.Token);
+                }
+                catch (Exception ex)
+                {
+                    if (ex is OperationCanceledException)
+                        MessageBox.Show("Successfully aborted");
+                    else
+                        MessageBox.Show($"ERROR: {ex.Message}");
+
+                    return;
+                }
+            }
+        }
+
+        private async void StartOldFunction(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{await MyOldFunctionAsync(400)}");
+        }
+
+        public Task<long> MyOldFunctionAsync(int anyNumber)
+        {
+            return Task.Run(() => MyOldFunction(anyNumber));
+        }
+
+        public long MyOldFunction(int anyNumber)
+        {
+            Thread.Sleep(5000);
+            return anyNumber * 3;
         }
     }
 }
