a53e360246b0c771b023c1d04b4f4e13029b8a94
André <AndreR@outlook.de>
2022-04-05 16:53:15 +0200

    WebApi with Pizza

---
+++ PizzaService/PizzaService/Controllers/PizzaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        static List<Pizza> pizzaDb = new List<Pizza>();
+
+        static PizzaController()
+        {
+            pizzaDb.Add(new Pizza() { Id = 1, Name = "Salami", Price = 9.5m });
+            pizzaDb.Add(new Pizza() { Id = 2, Name = "Ham", Price = 9.3m });
+            pizzaDb.Add(new Pizza() { Id = 3, Name = "Fishfingers", Price = 12.5m });
+        }
+
+        [HttpPut]
+        public void Update(Pizza pizza)
+        {
+            Delete(pizza.Id);
+            pizzaDb.Add(pizza);
+        }
+
+
+        [HttpDelete()]
+        public void Delete(int id)
+        {
+            pizzaDb.Remove(pizzaDb.First(x => x.Id == id));
+        }
+
+
+        [HttpPost()]
+        public void Add(Pizza pizza)
+        {
+            pizzaDb.Add(pizza);
+        }
+
+        [HttpGet()]
+        public IEnumerable<Pizza> Get()
+        {
+            return pizzaDb;
+        }
+    }
+
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
